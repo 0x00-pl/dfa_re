c8aa1e1f62739372e94a9ebf5e33a30eefb15a3f
pl <0x00.pl@gmail.com>
2014-12-22 22:13:09 +0800

    fix re.h

--- src/re.h
+++ src/re.h
@@ -23,32 +23,42 @@ namespace dfa_re{
       dfa::build_dfa(nfa,nfa_end_state_set, dfa,dfa_end_state_set);
     }
     size_t match_step(size_t state, string& s, size_t& pos){
+#ifdef PL_DEBUG
+      cout<<"step "<<state<<" --> ";
+#endif
       auto adj= dfa.adj(state);
       for(size_t i=0; i<adj.size(); i++){
 	graph::edge* e= dfa.edges[adj[i]];
 	if(dynamic_cast<graph::char_edge*>(e)!=nullptr){
-	  char ch= s[pos];
 	  graph::char_edge* ce= dynamic_cast<graph::char_edge*>(e);
-	  if(ce.match(ch)){
-	    pos++;
+	  if(ce->match(s,pos)){
+#ifdef PL_DEBUG
+      cout<<ce->to<<endl;
+#endif
 	    return ce->to;
 	  }
 	}else{
 	  throw "unknow";
 	}
       }
+#ifdef PL_DEBUG
+      cout<<"fail"<<endl;
+#endif
+      return dfa.sum_v();
     }
-    bool match_to_end_state(string& s, size_t& pos){
+    bool match_to_end_state(string s, size_t& pos){
       size_t state=0;
-      while(pos<s.size() && dfa_end_state_set.find(state)!=dfa_end_state_set.end()){
+      while(pos<s.size() && dfa_end_state_set.find(state)==dfa_end_state_set.end()){
 	state= match_step(state, s, pos);
+	if(state==dfa.sum_v()){return false;}
       }
       return dfa_end_state_set.find(state) != dfa_end_state_set.end();
     }
-    size_t match_to_pos(string& s, size_t& pos, size_t end_pos){
+    size_t match_to_pos(string s, size_t& pos, size_t end_pos){
       size_t state=0;
       while(pos<s.size() && pos!=end_pos){
 	state= match_step(state, s, pos);
+	if(state==dfa.sum_v()){return false;}
       }
       return state;
     }
