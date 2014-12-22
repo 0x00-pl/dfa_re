9d99c91e10f1f7d8e6515b8ed173f6d8961e5585
pl <0x00.pl@gmail.com>
2014-12-22 22:02:14 +0800

    quick commit:
    /home/pl/Documents/kdevelop/dfa_re/main.cpp
    
    %s

--- main.cpp
+++ main.cpp
@@ -1,28 +1,14 @@
 #include <iostream>
-#include "src/config.h"
-#include "src/parser.h"
-#include "src/nfa.h"
-#include "src/dfa.h"
-
+#include "src/re.h"
 using namespace std;
-using namespace parser;
 
 
 int main(int argc, char **argv) {
   (void)argc;(void)argv;
   std::cout << "Hello, world!" << std::endl;
-  
-  node* parsed= parser_str("(aaa|aab)b");
-  parsed->print(cout);
-  cout<<endl;
-  
-  graph::graph nfa;
-  nfa.print(cout);
-  set<size_t> nfa_end_state_set;
-  nfa::build_nfa(nfa,parsed,nfa_end_state_set);
-  
-  graph::graph dfa;
-  set<size_t> dfa_end_state_set;
-  dfa::build_dfa(nfa,nfa_end_state_set, dfa,dfa_end_state_set);
+  dfa_re::regex test_patt(".*(aaa|aab)*c");
+  size_t pos= 0;
+  bool match_result= test_patt.match_to_end_state("aaaabc",pos);
+  cout<<"match result:"<< match_result<<endl;
   return 0;
 }
